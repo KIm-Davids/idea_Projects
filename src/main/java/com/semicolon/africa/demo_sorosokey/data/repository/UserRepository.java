f20cb448c3640bfc973eb468050e6d5e17878119
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-07-23 01:50:48 +0100

    Tonights Code

---
+++ src/main/java/com/semicolon/africa/demo_sorosokey/data/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.semicolon.africa.demo_sorosokey.data.repository;
+
+public class UserRepository {
+}
--- src/main/java/com/semicolon/africa/demo_sorosokey/service/UserServices.java
+++ src/main/java/com/semicolon/africa/demo_sorosokey/service/UserServices.java
@@ -1,14 +1,11 @@
 package com.semicolon.africa.demo_sorosokey.service;
 
-import com.semicolon.africa.demo_sorosokey.dto.CommentRequest;
-import com.semicolon.africa.demo_sorosokey.dto.CommentResponse;
-import com.semicolon.africa.demo_sorosokey.dto.UserRequest;
-import com.semicolon.africa.demo_sorosokey.dto.UserResponse;
+import com.semicolon.africa.demo_sorosokey.dto.*;
 
 public interface UserServices {
 
     UserResponse signUp(UserRequest userRequest);
-    CommentResponse commentToPost(CommentRequest commentRequest)
-    CommentResponse deleteCOmment(CommentResponse commentResponse)
-    SharePostResponse sharePost (SharePostRequest sharePostRequest)
+    CommentResponse commentToPost(CommentRequest commentRequest);
+    CommentResponse deleteCOmment(CommentResponse commentResponse);
+    SharePostResponse sharePost (SharePostResponse sharePostRequest);
 }
